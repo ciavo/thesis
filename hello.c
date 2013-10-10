634bd93f9f0a86b9235c69401ec92f768dbcd88c
ciavo <luca.ciavaglia@gmail.com>
2013-10-10 11:45:33 +0200

    Up hello.c file. Checking parameters: VALUE, ANY_VALUE, STRING, USER_FUNCTION

--- hello.c
+++ hello.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include "hello.h"
 
-void extfunc_1(int x);
-void extfunc_2(int x);
-int main(int argc, char **argv)
+void extfunc1(int x);
+void extfunc3(double param1, long double param2);
+void extfunc4(char* stringa);
+
+int extfunc2(double x, int y);
+
+/*int main(int argc, char **argv)
 {
+  function(4,5);
+  }*/
 
-  int x =1;
-  //printf("Hello, world!");
-  //extfunc_1(1); 
-  //extfunc_2(2);
+int function(int x, int y)
+{
+  int w = 2 + extfunc2(5.4, 27);
+  extfunc1(51);
+  extfunc4("Hello!");
+  return x;
+  // return extfunc_1(4); Check!
 }
 
-void functio(){
-  extfunc_1(3);
- // extfunc_1(3);
-}
+
+
+
